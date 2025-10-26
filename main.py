6428902be3278fe696ec18bdc8ec0ec18e32372f
cdao2002 <cdao@students.avc.edu>
2025-10-25 21:16:34 -0700

    commit 1st

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def getPivot(number):
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    n = len(number)
+    if n == 0:
+        raise ValueError("number must not be empty")
+    
+    avg = sum(number) / n
+    pivot_idx = 0
+    best_dist = abs(number[0] - avg)
+    
 
 def split(number):
     """
